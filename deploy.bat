cb894d3d2c0524193f8bbf1b8d0bf517b303fa57
avraCodabra <751tcy751tcy>
2021-09-21 16:04:05 +0300

    Clickable UI

--- deploy.bat
+++ deploy.bat
@@ -5,5 +5,5 @@ cd dist
 git init
 git add -A
 git commit -m "deploy"
-git push -f git@github.com:AvraCodabra\vue-parking.git master:gh-pages
+git push -f git@github.com:AvraCodabra/vue-parking.git master:gh-pages
 cd -
