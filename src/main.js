d51aba54d74d72cd396f72548870e1269822ce0f
talbenavraham <tal.benavraham@bigpanda.io>
2022-06-11 16:10:54 +0300

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics';
+import router from "router";
 
 createApp(App).mount('#app')
+
+Vue.use(VueAnalytics, {
+    id: 'G-DBXTX64G9V',
+    router
+})
